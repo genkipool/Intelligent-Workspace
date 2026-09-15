b93a62a3dbb81dd62b808aa9d8e74733e2962589
Genkipool <genkipoolcardano@gmail.com>
2026-09-15 11:23:07 +0200

    fix(about): eliminar permisos commands y windows de la web about

--- src/ui/pages/about/components/PermissionsSection.svelte
+++ src/ui/pages/about/components/PermissionsSection.svelte
@@ -11,14 +11,12 @@
         <li><strong translate="no">tabGroups:</strong> <span>{$t('permTabGroups')}</span></li>
         <li><strong translate="no">storage:</strong> <span>{$t('permStorage')}</span></li>
         <li><strong translate="no">favicon:</strong> <span>{$t('permFavicon')}</span></li>
-        <li><strong translate="no">commands:</strong> <span>{$t('permCommandsDesc')}</span></li>
         <li><strong translate="no">contextMenus:</strong> <span>{$t('permContextMenus')}</span></li>
         <li><strong translate="no">notifications:</strong> <span>{$t('permNotifications')}</span></li>
         <li><strong translate="no">sidePanel:</strong> <span>{$t('permSidePanel')}</span></li>
         <li><strong translate="no">scripting:</strong> <span>{$t('permScripting')}</span></li>
         <li><strong translate="no">downloads:</strong> <span>{$t('permDownloads')}</span></li>
         <li><strong translate="no">system.display:</strong> <span>{$t('permSystemDisplay')}</span></li>
-        <li><strong translate="no">windows:</strong> <span>{$t('permWindows')}</span></li>
         <li>
             <strong translate="no">declarativeNetRequestWithHostAccess:</strong>
             <span>{$t('permDeclarativeNetRequestWithHostAccess')}</span>
